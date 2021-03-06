303514fe358ab17461b766f84fb6401268a1df27
Beau Brown <bbrown@fusionalliance.com>
2021-03-06 10:21:08 -0500

    fun stuff

--- conftest.py
+++ conftest.py
@@ -2,5 +2,11 @@ import pytest
 from chromedriver_py import binary_path
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path=binary_path)
-driver.implicitly_wait(5)
+@pytest.fixture(name="driver", scope="class")
+def driver(request):
+    driver = webdriver.Chrome(executable_path=binary_path)
+    driver.implicitly_wait(5)
+    driver.maximize_window()
+
+    yield driver
+    driver.close()
--- pages/landing_page.py
+++ pages/landing_page.py
@@ -1,9 +1,12 @@
 from selenium.webdriver.common.by import By
+from src.testproject.sdk.drivers import webdriver
+
 
 class LandingPage:
     url = 'https://littlecaesars.com/en-us/'
 
     link_logo = (By.XPATH, "(//a[@data-testid = 'header__littleCaesars-logo'])[2]")
+    div_start_online_order = (By.XPATH, "//div[contains(text(), 'Start Online Order')]")
 
     def __init__(self, driver):
         self._driver = driver
@@ -14,4 +17,8 @@ class LandingPage:
 
     def link_logo_displayed(self):
         if self._driver.find_element(*self.link_logo).is_displayed():
+            return True
+
+    def div_start_online_order_displayed(self):
+        if self._driver.find_element(*self.div_start_online_order).is_displayed():
             return True
