473e0484fc1fd384d443b0931c78df037f933d29
Beau Brown <bbrown@fusionalliance.com>
2021-04-07 20:57:01 -0400

    help

--- conftest.py
+++ conftest.py
@@ -1,10 +1,12 @@
 import pytest
 from chromedriver_py import binary_path
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 @pytest.fixture(name="driver", scope="class")
 def driver(request):
-    driver = webdriver.Chrome(executable_path=binary_path)
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.implicitly_wait(5)
     driver.maximize_window()
 
